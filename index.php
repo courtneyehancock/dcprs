f8e3fbd574fe77d90449480189fb3dacb9ff69db
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-09-17 10:49:34 -0400

    Exchange index.php code for tested code

--- index.php
+++ index.php
@@ -1,23 +1,8 @@
 <?php get_header(); ?>
 
 <div class="main-content">
-  <div class="internal-div">
-    <?php dynamic_sidebar('banner-home'); ?>
-    <h1 class="text-center pt-5">CHECK HERE FOR ALL FLAWARN UPDATES</h1>
-    <hr>
-    <div class="container d-flex p-4 justify-content-center align-content-center align-items-end">
-  <h2 class="pr-3">Looking for something specific?</h2><form role="search" method="get" id="searchform" class="searchform" action="https://pwd.aa.ufl.edu/">
-      <div>
-        <label class="screen-reader-text" for="s">Search for:</label>
-        <input type="text" value="" name="s" id="s">
-        <input type="submit" id="searchsubmit" value="Search">
-      </div>
-    </form>
-    </div>
-  </div>
-
   <div class="container-fluid">
-    <div class="post-container row justify-content-center">
+    <div class="row align-items-start internal-div justify-content-center">
     <?php
       if(have_posts()){
         while(have_posts()){
@@ -25,7 +10,7 @@
 
           <div class="col-lg-3 posts">
             <?php the_post_thumbnail('medium'); ?>
-            <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <h2 class="post-title"><a class="aa" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
             <?php the_excerpt(); ?>
             <?php
               $archive_year = get_the_time('Y');
@@ -33,9 +18,8 @@
               $archive_day = get_the_time('d');
             ?>
             <div class="post-info">
-              <p class="font-italic">Published: <?php echo get_the_date(); ?></p>
-              <a class="btn mt-3" href="<?php the_permalink(); ?>">LEARN MORE</a>
-            <!--  <p class="category-label font-italic">Category: <?php the_category(); ?></p>-->
+            <!--  <p class="font-italic">Published: <a href="<?php echo get_day_link($archive_year, $archive_month, $archive_day); ?>"><?php echo get_the_date(); ?></a></p>-->
+              <p class="category-label font-italic">Category: <?php the_category(); ?></p>
             </div>
           </div>
     <?php } //ends while loop

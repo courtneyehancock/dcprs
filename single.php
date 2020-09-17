0dfac14c0d64ddcdc708c37e2fd8acefa57da97d
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-09-17 11:07:46 -0400

    Edit single post

--- single.php
+++ single.php
@@ -6,14 +6,22 @@
     if(have_posts()){
       while(have_posts()){
         the_post(); ?>
-        <div class="post-feature">
+      <!--  <div class="post-feature">
           <?php the_post_thumbnail(); ?>
-        </div>
-        <h2 class="post-title"><?php the_title(); ?></h2>
-        <p class="published"><?php echo "Published: " . get_the_date(); ?></p>
-        <?php the_content(); ?>
+        </div>-->
+        <!--<h2 class="post-title"><?php the_title(); ?></h2>-->
+        <!--<p><?php echo "Published: " . get_the_date(); ?></p>-->
+        <!--<p><?php echo "This post is written by : " . get_the_author(); ?></p>-->
+          <?php the_content(); ?>
 
-      </div>
+        <div class="pagination-single">
+          <?php previous_post_link('%link', '<< Previous Post');?>
+        </div>
+
+        <div class="pagination-single">
+          <?php next_post_link('%link', 'Next Post >>');?>
+        </div>
+
         <?php
       }
     }
